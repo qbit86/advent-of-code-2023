30cc7ec3c5e3b0f633989b46b9a33fd454dd73c7
Viktor Ptitselov <qbit86@gmail.com>
2023-12-09 00:42:34 +0200

    Add failing test case

--- AdventOfCode2023-CSharp/tests/Tests.Puzzles/PartTwoPuzzle_Tests.cs
+++ AdventOfCode2023-CSharp/tests/Tests.Puzzles/PartTwoPuzzle_Tests.cs
@@ -7,6 +7,9 @@ public sealed class PartTwoPuzzle_Tests
 {
     [Theory]
     [InlineData("sample-3.txt", 6L)]
+#if false
+    [InlineData("sample-4.txt", 3L)]
+#endif
     [InlineData("input.txt", 10241191004509L)]
     internal async Task SolveAsync(string inputPath, long expected)
     {
