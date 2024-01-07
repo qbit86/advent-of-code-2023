2384475c1aec256f62a13f9035b9235bdc3d457f
Viktor Ptitselov <qbit86@gmail.com>
2024-01-07 21:20:39 +0200

    Reimplement using `Arborescence.Search.Adjacency.AdditiveEnumerableDijkstra<AdventOfCode2023.Node, int>`

--- AdventOfCode2023-CSharp/src/AdventOfCode2023.Puzzles/Helpers.cs
+++ AdventOfCode2023-CSharp/src/AdventOfCode2023.Puzzles/Helpers.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Linq;
 using Arborescence;
 using Arborescence.Models;
+using Dijkstra = Arborescence.Search.Adjacency.AdditiveEnumerableDijkstra<AdventOfCode2023.Node, int>;
 
 namespace AdventOfCode2023;
 
@@ -14,34 +15,16 @@ internal static class Helpers
         TLines lines, int rowCount, int columnCount, TGraph graph,
         Predicate<Node> isDestination)
         where TLines : IReadOnlyList<string>
-        where TGraph : IForwardIncidence<Node, Endpoints<Node>, IncidenceEnumerator<Node, IEnumerator<Node>>>
+        where TGraph : IOutNeighborsAdjacency<Node, IEnumerator<Node>>
     {
         Debug.Assert(lines is not null);
         Debug.Assert(lines.Count > 0);
 
         var heatLossMap = HeatLossMap.Create(lines);
         Node source = new(Point.Empty, Size.Empty, 0);
-        Dictionary<Node, int> distanceByNode = new(rowCount * columnCount) { [source] = 0 };
-        PriorityQueue<Node, int> frontier = new();
-        frontier.Enqueue(source, 0);
-        while (frontier.TryDequeue(out Node current, out int priority))
-        {
-            int currentDistance = distanceByNode[current];
-            if (priority > currentDistance)
-                continue;
-            IncidenceEnumerator<Node, IEnumerator<Node>> edges = graph.EnumerateOutEdges(current);
-            foreach (Endpoints<Node> edge in edges)
-            {
-                Node neighbor = edge.Head;
-                int weight = heatLossMap.GetValueOrDefault(edge);
-                int neighborDistanceCandidate = currentDistance + weight;
-                if (neighborDistanceCandidate < distanceByNode.GetValueOrDefault(neighbor, int.MaxValue))
-                {
-                    distanceByNode[neighbor] = neighborDistanceCandidate;
-                    frontier.Enqueue(neighbor, neighborDistanceCandidate);
-                }
-            }
-        }
+        Dictionary<Node, int> distanceByNode = new(rowCount * columnCount);
+        IEnumerable<Endpoints<Node>> edges = Dijkstra.EnumerateEdges(graph, source, heatLossMap, distanceByNode);
+        foreach (Endpoints<Node> _ in edges) { }
 
         IEnumerable<KeyValuePair<Node, int>> nodeDistancePairs = distanceByNode.Where(kv => isDestination(kv.Key));
         int result = nodeDistancePairs.Min(kv => kv.Value);
